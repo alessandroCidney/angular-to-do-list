534444f1e69634d2604fbf6e34812c2f7f2559d9
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2023-08-19 21:20:17 -0300

    Hands-on 8

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -8,6 +8,19 @@ import { TaskList } from '../../model/task-list'
 })
 export class TodoListComponent {
   public taskList: Array<TaskList> = [
-    { task: 'Minha nova task', checked: true }
+    { task: 'Minha nova task', checked: true },
+    { task: 'Minha nova task 2', checked: false }
   ]
+
+  public deleteItemTaskList (index: number) {
+    this.taskList.splice(index, 1)
+  }
+
+  public deleteAllTaskList () {
+    const confirm = window.confirm('Você realmente deseja deletar todos os itens?')
+
+    if (confirm) {
+      this.taskList = []
+    }
+  }
 }
